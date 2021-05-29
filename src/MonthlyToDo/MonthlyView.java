a507abd5485f3bd609a996edf58b7ac997a44a22
StevenChang805 <changs@isyedu.org>
2021-05-29 12:33:52 +0630

    initial attempt to load monthlyview.fxml

--- src/MonthlyToDo/MonthlyView.java
+++ src/MonthlyToDo/MonthlyView.java
@@ -1,5 +1,24 @@
 package MonthlyToDo;
 
-public class MonthlyView {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.util.Objects;
+
+public class MonthlyView extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("My First JavaFX App");
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Point reached.");
+        Application.launch(args);
+    }
 }
--- src/base/Main.java
+++ src/base/Main.java
@@ -1,23 +1,26 @@
 package base;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+    private static Stage primaryStage;
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        primaryStage = stage;
+        Parent root = FXMLLoader.load(getClass().getResource("../MonthlyToDo/MonthlyToDo.fxml"));
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
 }
