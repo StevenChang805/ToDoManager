91f6f390aeba4a29f46ae718528539225990ccd0
StevenChang805 <changs@isyedu.org>
2021-08-08 09:04:42 +0630

    created DataCollector class

--- src/base/DataCollector.java
+++ src/base/DataCollector.java
@@ -1,5 +1,45 @@
 package base;
 
+import java.sql.ResultSet;
+import java.time.LocalDate;
+
 public class DataCollector {
-    
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public DataCollector() {
+
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public ResultSet doQuery(String query) {
+
+    }
+
+    public String createQuery(LocalDate startDate, LocalDate endDate) {
+
+    }
+
+    public String connectToDatabase() {
+
+    }
+
+    public String updateDatabase(String sql) {
+
+    }
+
+    public void setStartDate(LocalDate startDate) {
+
+    }
+
+    public void setEndDate(LocalDate endDate) {
+
+    }
 }
