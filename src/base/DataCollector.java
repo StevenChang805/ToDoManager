cb099457114c557265c43abeec9d28df231b2cad
StevenChang805 <changs@isyedu.org>
2021-08-23 08:22:58 +0630

    updateToDatabase() implemented
    finished method to insert new items into database

--- src/base/DataCollector.java
+++ src/base/DataCollector.java
@@ -1,20 +1,14 @@
 package base;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Properties;
 
 public class DataCollector {
     private Connection conn;
-
-    private LocalDate startDate;
-    private LocalDate endDate;
 
     public DataCollector() {
         String error = connectToDatabase();
@@ -24,20 +18,54 @@ public class DataCollector {
         }
     }
 
-    public LocalDate getStartDate() {
-        return startDate;
+    public PreparedStatement prepareStatement(String query){
+        try {
+            PreparedStatement st = conn.prepareStatement(query);
+            return st;
+        } catch(SQLException e) {
+            System.out.println("An error occurred while trying to connect to the database.");
+            return null;
+        }
     }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-//    public ResultSet doQuery(String query) {
-//
-//    }
 
     public String createQuery(LocalDate startDate, LocalDate endDate) {
-        return "Work in Progress.";
+        String start_date = localDateToDateTime(startDate);
+        String end_date = localDateToDateTime(endDate);
+
+        String query = "SELECT type, name, description, date FROM items WHERE date >= "
+                + start_date + " AND date <= " + end_date + " ORDER BY date";
+
+        return query;
+    }
+
+    public String localDateToDateTime(LocalDate date) {
+        String year = date.getYear()+"";
+        String month = prefixNumber(date.getMonthValue());
+        String day = prefixNumber(date.getDayOfMonth());
+        return year+month+day;
+    }
+
+    public String localDateTimeToDateTime(LocalDateTime dateTime) {
+        // using existing method to create date string
+        LocalDate date = dateTime.toLocalDate();
+        String date_string = localDateToDateTime(date);
+
+        // creating time string
+        String hour = prefixNumber(dateTime.getHour());
+        String minute = prefixNumber(dateTime.getMinute());
+        String second = prefixNumber(dateTime.getSecond());
+        String time_string = hour+":"+minute+":"+second;
+
+        return date_string + " " + time_string;
+    }
+
+    public String prefixNumber(int num) {
+        // prefixes inputted number with a 0 if less than 10
+        if (num < 10) {
+            return "0"+num;
+        } else {
+            return Integer.toString(num);
+        }
     }
 
     public String connectToDatabase() {
@@ -71,15 +99,31 @@ public class DataCollector {
         return "";
     }
 
-//    public String updateDatabase(String sql) {
-//
-//    }
+    public void updateDatabase(int type, String name, String description, LocalDate date,
+                                 LocalDateTime start_time, LocalDateTime end_time, int complete) {
+        String date_string = localDateToDateTime(date);
+        String start_time_string = "null";
+        String end_time_string = "null";
+        if (start_time != null) {
+            start_time_string = localDateTimeToDateTime(start_time);
+        }
+        if (end_time != null) {
+            end_time_string = localDateTimeToDateTime(end_time);
+        }
+        String query = "INSERT INTO items (type, name, description, date, start_time, end_time, complete) VALUES ("
+                + type
+                + ", \"" + name + "\", "
+                + "\"" + description + "\", "
+                + date_string + ", "
+                + start_time_string + ", "
+                + end_time_string + ", "
+                + complete + ");";
 
-    public void setStartDate(LocalDate startDate) {
-
-    }
-
-    public void setEndDate(LocalDate endDate) {
-
+        try {
+            prepareStatement(query).executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("There was an issue executing the update.");
+            e.printStackTrace();
+        }
     }
 }
--- src/base/Main.java
+++ src/base/Main.java
@@ -8,10 +8,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Main extends Application {
 
     public static Stage primaryStage;
     public MonthlyViewController mvc;
+
+    // int type, String name, String description, LocalDate date,
+    //                                 LocalDateTime start_time, LocalDateTime end_time, int complete
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -19,6 +25,8 @@ public class Main extends Application {
         // initialize the application with monthly view
         Parent root = FXMLLoader.load(getClass().getResource("../MonthlyToDo/MonthlyToDo.fxml"));
         DataCollector dc = new DataCollector();
+        dc.updateDatabase(1, "Computer Science IA", "Finish programming", LocalDate.of(2021, 10, 15),
+                null, null, 0);
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
